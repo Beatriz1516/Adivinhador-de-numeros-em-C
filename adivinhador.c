d8cb62ed1991d88959e12cf8770d453a7508a685
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-06-17 09:14:47 -0300

    Adicionado o .gitignore;

--- adivinhador.c
+++ adivinhador.c
@@ -7,7 +7,13 @@ int main() {
 
     //entrada
     int numero_secreto = 89;
-    
+
+    int chute_numero;
+    printf("Qual é o seu palpite?")
+    scanf("%d", &chute_numero);
+    printf("Seu chute foi %d", chute_numero);
+
     //processamento
     //saída
 } 
+
