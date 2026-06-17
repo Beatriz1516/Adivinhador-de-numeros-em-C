8e886215f4cf2834a400d670d4f26d9451d15621
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-06-17 08:30:56 -0300

    primeiro commit

---
+++ adivinhador.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    // Cabeçalho
+    printf("==================================================\n");
+    printf("=     Bem-vindo ao nosso jogo de adivinhação     =\n");
+    printf("==================================================\n");
+
+    //entrada
+    int numero_secreto = 89;
+    
+    //processamento
+    //saída
+} 
